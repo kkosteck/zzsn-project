237f3e36e86f3d2ac12ab95bcbb669fc24bf2d9c
Kacper Kostecki <01143577@pw.edu.pl>
2022-05-28 23:24:30 +0200

    added validation during training

--- SNN_supervised.py
+++ SNN_supervised.py
@@ -15,59 +15,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.manual_seed_all(0)
 torch.set_num_threads(os.cpu_count() - 1)
 
-import IPython
-import numpy as np
-from IPython.display import display, HTML, Javascript
-import time
-import random
-
-# google colab plotting
-def configure_browser_state():
-  display(IPython.core.display.HTML('''
-    <canvas id="myChart"></canvas>
-  '''))
-  display(IPython.core.display.HTML('''
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-        <script>
-          var ctx = document.getElementById('myChart').getContext('2d');
-          var chart = new Chart(ctx, {
-              // The type of chart we want to create
-              type: 'line',
-
-              // The data for our dataset
-              data: {
-                  labels: [0,1,2,3,4,5],
-                  datasets: [{
-                      label: 'Score',
-                      borderColor: 'rgb(255, 99, 132)',
-                      data: [0,1,2,3,4,5]
-                  }]
-              },
-
-              // Configuration options go here
-              options: {
-                animation: {
-                  duration: 0, // general animation time
-                }
-              }
-          });
-
-          function addData(label, value){
-            chart.data.labels.push(label)
-            chart.data.datasets[0].data.push(value)
-
-            // optional windowing
-            if(chart.data.labels.length > 10) {
-              chart.data.labels.shift()
-              chart.data.datasets[0].data.shift()
-            }
-
-            chart.update();
-          }
-        </script>
-        '''))
-
-
 
 def load_config(filename):
     with open(filename) as f:
@@ -98,81 +45,118 @@ def create_monitorings(network: DiehlAndCook2015, time: int):
 
     return network, spikes
 
-def train_network(network, dataset, spikes, n_train, update_interval, n_classes, time, n_neurons, plot=False):
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
-    spike_record = torch.zeros(update_interval, time, n_neurons, device=device)
+def train_network(network, train_set, val_set, spikes, config):
+    dataloader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=True)
+    spike_record = torch.zeros(config["update_interval"], config["time"], config["n_neurons"], device=device)
     accuracy = {"all": [], "proportion": []}
-    labels = torch.empty(update_interval, device=device)
-    per_class = int(n_neurons / n_classes)
+    labels = torch.empty(config["update_interval"], device=device)
+    per_class = int(config["n_neurons"] / config["n_classes"])
 
-    assignments = -torch.ones_like(torch.Tensor(n_neurons), device=device)
-    proportions = torch.zeros_like(torch.Tensor(n_neurons, n_classes), device=device)
-    rates = torch.zeros_like(torch.Tensor(n_neurons, n_classes), device=device)
+    assignments = -torch.ones_like(torch.Tensor(config["n_neurons"]), device=device)
+    proportions = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
+    rates = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
     # Train the network.
     print("Begin training.\n")
 
-    pbar = None
-    if not plot:
-        pbar = tqdm(total=n_train)
+    pbar = tqdm(total=config["n_train"], position=0, leave=True)
     for (i, datum) in enumerate(dataloader):
-        if i > n_train:
+        if i > config["n_train"]:
             break
 
         image = datum["encoded_image"]
         label = datum["label"]
 
-        if i % update_interval == 0 and i > 0:
+        if i % config["update_interval"] == 0 and i > 0:
             # Get network predictions.
-            all_activity_pred = all_activity(spike_record, assignments, n_classes)
-            proportion_pred = proportion_weighting(spike_record, assignments, proportions, n_classes)
+            all_activity_pred = all_activity(spike_record, assignments, config["n_classes"])
+            proportion_pred = proportion_weighting(spike_record, assignments, proportions, config["n_classes"])
 
             # Compute network accuracy according to available classification strategies.
-            accuracy["all"].append(100 * torch.sum(labels.long() == all_activity_pred).item() / update_interval)
-            accuracy["proportion"].append(100 * torch.sum(labels.long() == proportion_pred).item() / update_interval)
+            accuracy["all"].append(100 * torch.sum(labels.long() == all_activity_pred).item() / config["update_interval"])
+            accuracy["proportion"].append(100 * torch.sum(labels.long() == proportion_pred).item() / config["update_interval"])
 
             #print(f"\nAll activity accuracy: {accuracy['all'][-1]:.2f} (last), {np.mean(accuracy['all']):.2f} (average), {np.max(accuracy['all']):.2f} (best)")
             #print(f"Proportion weighting accuracy: {accuracy['proportion'][-1]:.2f} (last), {np.mean(accuracy['proportion']):.2f} (average), {np.max(accuracy['proportion']):.2f} (best)\n")
 
             # Assign labels to excitatory layer neurons.
-            assignments, proportions, rates = assign_labels(spike_record, labels, n_classes, rates)
-            if plot:
-                display(Javascript(f"addData({i},{np.mean(accuracy['all']):.2f})"))
+            assignments, proportions, rates = assign_labels(spike_record, labels, config["n_classes"], rates)
+        if i% config["eval_interval"] == 0 and i > 0:
+            acc = validate_network(network, val_set, spikes, config)
 
         # Add the current label to the list of labels for this update_interval
-        labels[i % update_interval] = label[0]
+        labels[i % config["update_interval"]] = label[0]
 
         # Run the network on the input.
-        choice = np.random.choice(int(n_neurons / n_classes), size=1, replace=False)
+        choice = np.random.choice(int(config["n_neurons"] / config["n_classes"]), size=1, replace=False)
         clamp = {"Ae": per_class * label.long() + torch.Tensor(choice).long()}
-        inputs = {"X": image.cuda().view(time, 1, 1, 28, 28)}
-        network.run(inputs=inputs, time=time, clamp=clamp)
+        inputs = {"X": image.cuda().view(config["time"], 1, 1, 28, 28)}
+        network.run(inputs=inputs, time=config["time"], clamp=clamp)
 
         # Add to spikes recording.
-        spike_record[i % update_interval] = spikes["Ae"].get("s").view(time, n_neurons)
+        spike_record[i % config["update_interval"]] = spikes["Ae"].get("s").view(config["time"], config["n_neurons"])
 
         network.reset_state_variables()  # Reset state variables.
-        if not plot:
-            pbar.set_description_str(f"Accuracy: {np.mean(accuracy['all']):.2f}. Train progress: ")
-            pbar.update()
+        pbar.set_description_str(f"Accuracy: {np.mean(accuracy['all']):.2f}. Train progress: ")
+        pbar.update()
 
-    print(f"Progress: {n_train} / {n_train} \n")
+    print(f"Progress: {config['n_train']} / {config['n_train']} \n")
     print("Training complete.\n")
     return network
 
-# do przerobienia
-def test_network(network, config, spikes):
+def validate_network(network, val_set, spikes, config):
+    print("Evaluate network...")
+    # Sequence of accuracy estimates.
+    accuracy = {"all": 0, "proportion": 0}
+
+    # Record spikes during the simulation.
+    spike_record = torch.zeros(1, int(config["time"] / config["dt"]), config["n_neurons"], device=device)
+
+    network.train(mode=False)
+
+    assignments = -torch.ones_like(torch.Tensor(config["n_neurons"]), device=device)
+    proportions = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
+
+    pbar = tqdm(total=config["n_eval"], position=0, leave=True)
+    for step, batch in enumerate(val_set):
+        if step > config["n_eval"]:
+            break
+        # Get next input sample.
+        inputs = {"X": batch["encoded_image"].view(int(config["time"] / config["dt"]), 1, 1, 28, 28)}
+        inputs = {k: v.cuda() for k, v in inputs.items()}
+
+        # Run the network on the input.
+        network.run(inputs=inputs, time=config["time"], input_time_dim=1)
+
+        # Add to spikes recording.
+        spike_record[0] = spikes["Ae"].get("s").squeeze()
+
+        # Convert the array of labels into a tensor
+        label_tensor = torch.tensor(batch["label"], device=device)
+
+        # Get network predictions.
+        all_activity_pred = all_activity(spikes=spike_record, assignments=assignments, n_labels=config["n_classes"])
+        proportion_pred = proportion_weighting(
+            spikes=spike_record,
+            assignments=assignments,
+            proportions=proportions,
+            n_labels=config["n_classes"],
+        )
+
+        # Compute network accuracy according to available classification strategies.
+        accuracy["all"] += float(torch.sum(label_tensor.long() == all_activity_pred).item())
+        accuracy["proportion"] += float(torch.sum(label_tensor.long() == proportion_pred).item())
+
+        network.reset_state_variables()  # Reset state variables.
+
+        pbar.set_description_str(f"Accuracy: {(max(accuracy['all'] ,accuracy['proportion'] ) / (step+1)):.3}")
+        pbar.update()
+    
+    network.eval()
+    return accuracy['all'] / config['n_eval']
+    
+
+def test_network(network, config, spikes, test_dataset):
     print("Testing....\n")
-    # Load MNIST data.
-    test_dataset = MNIST(
-        PoissonEncoder(time=config["time"], dt=config["dt"]),
-        None,
-        root=os.path.join("..", "..", "data", "MNIST"),
-        download=True,
-        train=False,
-        transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Lambda(lambda x: x * config["intensity"])]
-        ),
-    )
 
     # Sequence of accuracy estimates.
     accuracy = {"all": 0, "proportion": 0}
@@ -188,7 +172,7 @@ def test_network(network, config, spikes):
     proportions = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
     rates = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
 
-    pbar = tqdm(total=config["n_test"])
+    pbar = tqdm(total=config["n_test"], position=0, leave=True)
     for step, batch in enumerate(test_dataset):
         if step > config["n_test"]:
             break

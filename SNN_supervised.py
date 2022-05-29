27010683c3cf9c3cef3fd8dac6c98d79d6c6988e
Kacper Kostecki <01143577@pw.edu.pl>
2022-05-29 14:12:12 +0200

    some changes

--- SNN_supervised.py
+++ SNN_supervised.py
@@ -32,6 +32,7 @@ def create_network(n_neurons, exc, inh, dt, theta_plus, lr) -> DiehlAndCook2015:
         norm=78.4,
         theta_plus=theta_plus,
         inpt_shape=(1, 28, 28),
+        reduction = torch.sum
     )
     network.to("cuda")
 
@@ -58,6 +59,7 @@ def train_network(network, train_set, val_set, spikes, config):
     # Train the network.
     print("Begin training.\n")
 
+    network.train()
     pbar = tqdm(total=config["n_train"], position=0, leave=True)
     for (i, datum) in enumerate(dataloader):
         if i > config["n_train"]:
@@ -75,13 +77,11 @@ def train_network(network, train_set, val_set, spikes, config):
             accuracy["all"].append(100 * torch.sum(labels.long() == all_activity_pred).item() / config["update_interval"])
             accuracy["proportion"].append(100 * torch.sum(labels.long() == proportion_pred).item() / config["update_interval"])
 
-            #print(f"\nAll activity accuracy: {accuracy['all'][-1]:.2f} (last), {np.mean(accuracy['all']):.2f} (average), {np.max(accuracy['all']):.2f} (best)")
-            #print(f"Proportion weighting accuracy: {accuracy['proportion'][-1]:.2f} (last), {np.mean(accuracy['proportion']):.2f} (average), {np.max(accuracy['proportion']):.2f} (best)\n")
-
             # Assign labels to excitatory layer neurons.
             assignments, proportions, rates = assign_labels(spike_record, labels, config["n_classes"], rates)
         if i% config["eval_interval"] == 0 and i > 0:
             acc = validate_network(network, val_set, spikes, config)
+            print(f"\nValidation accuracy: {acc:.2f}")
 
         # Add the current label to the list of labels for this update_interval
         labels[i % config["update_interval"]] = label[0]
@@ -105,54 +105,43 @@ def train_network(network, train_set, val_set, spikes, config):
 
 def validate_network(network, val_set, spikes, config):
     print("Evaluate network...")
-    # Sequence of accuracy estimates.
-    accuracy = {"all": 0, "proportion": 0}
-
-    # Record spikes during the simulation.
-    spike_record = torch.zeros(1, int(config["time"] / config["dt"]), config["n_neurons"], device=device)
-
-    network.train(mode=False)
-
-    assignments = -torch.ones_like(torch.Tensor(config["n_neurons"]), device=device)
-    proportions = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
-
-    pbar = tqdm(total=config["n_eval"], position=0, leave=True)
-    for step, batch in enumerate(val_set):
-        if step > config["n_eval"]:
-            break
-        # Get next input sample.
-        inputs = {"X": batch["encoded_image"].view(int(config["time"] / config["dt"]), 1, 1, 28, 28)}
-        inputs = {k: v.cuda() for k, v in inputs.items()}
-
-        # Run the network on the input.
-        network.run(inputs=inputs, time=config["time"], input_time_dim=1)
-
-        # Add to spikes recording.
-        spike_record[0] = spikes["Ae"].get("s").squeeze()
-
-        # Convert the array of labels into a tensor
-        label_tensor = torch.tensor(batch["label"], device=device)
-
-        # Get network predictions.
-        all_activity_pred = all_activity(spikes=spike_record, assignments=assignments, n_labels=config["n_classes"])
-        proportion_pred = proportion_weighting(
-            spikes=spike_record,
-            assignments=assignments,
-            proportions=proportions,
-            n_labels=config["n_classes"],
-        )
-
-        # Compute network accuracy according to available classification strategies.
-        accuracy["all"] += float(torch.sum(label_tensor.long() == all_activity_pred).item())
-        accuracy["proportion"] += float(torch.sum(label_tensor.long() == proportion_pred).item())
-
-        network.reset_state_variables()  # Reset state variables.
-
-        pbar.set_description_str(f"Accuracy: {(max(accuracy['all'] ,accuracy['proportion'] ) / (step+1)):.3}")
-        pbar.update()
-    
     network.eval()
-    return accuracy['all'] / config['n_eval']
+    accuracy = {"all": 0, "proportion": 0}
+    with torch.no_grad():
+
+        spike_record = torch.zeros(1, int(config["time"] / config["dt"]), config["n_neurons"], device=device)
+        assignments = -torch.ones_like(torch.Tensor(config["n_neurons"]), device=device)
+        proportions = torch.zeros_like(torch.Tensor(config["n_neurons"], config["n_classes"]), device=device)
+
+        pbar = tqdm(total=config["n_eval"], position=0, leave=True)
+        for step, batch in enumerate(val_set):
+            if step > config["n_eval"]:
+                break
+            inputs = {"X": batch["encoded_image"].view(int(config["time"] / config["dt"]), 1, 1, 28, 28)}
+            inputs = {k: v.cuda() for k, v in inputs.items()}
+
+            network.run(inputs=inputs, time=config["time"], input_time_dim=1)
+            spike_record[0] = spikes["Ae"].get("s").squeeze()
+            label_tensor = torch.tensor(batch["label"], device=device)
+
+            all_activity_pred = all_activity(spikes=spike_record, assignments=assignments, n_labels=config["n_classes"])
+            proportion_pred = proportion_weighting(
+                spikes=spike_record,
+                assignments=assignments,
+                proportions=proportions,
+                n_labels=config["n_classes"],
+            )
+
+            accuracy["all"] += float(torch.sum(label_tensor.long() == all_activity_pred).item())
+            accuracy["proportion"] += float(torch.sum(label_tensor.long() == proportion_pred).item())
+
+            network.reset_state_variables() 
+
+            pbar.set_description_str(f"Accuracy: {(max(accuracy['all'] ,accuracy['proportion'] ) / (step+1)):.3}")
+            pbar.update()
+        
+    network.train()
+    return 100 * accuracy['all'] / config['n_eval']
     
 
 def test_network(network, config, spikes, test_dataset):
@@ -162,6 +151,7 @@ def test_network(network, config, spikes, test_dataset):
     accuracy = {"all": 0, "proportion": 0}
 
     # Record spikes during the simulation.
+    print(config["time"])
     spike_record = torch.zeros(1, int(config["time"] / config["dt"]), config["n_neurons"], device=device)
 
     # Train the network.
